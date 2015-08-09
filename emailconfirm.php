287d6f52d84b9a7582f709bd536d1030edeba147
Prashant <pkpbhardwaj729@gmail.com>
2015-08-09 23:29:58 +0530

    redirected to buzz after email confirmation

--- emailconfirm.php
+++ emailconfirm.php
@@ -14,7 +14,10 @@ if ($code==$db_code) {
 	$query = "UPDATE users SET confirmed = '1', confirm_code = '0'";
     $result = mysqli_query($conn, $query);
     if ($result && mysqli_affected_rows($conn) == 1) {
-       redirect_to("public/Inside/buzz.php");
+        $_SESSION["username"] = $username;
+        redirect_to("public/Inside/buzz.php");            
+    }
+       
     } else {
         $_SESSION["message"] = "Updation failed.";
     }
