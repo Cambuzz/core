57ae87c4b835df460390841ba689bf57ca37419b
Prastut <kr.prastut@gmail.com>
2015-08-09 21:16:43 +0530

    Fixed UI errors on buzz.php

--- public/Inside/buzz.php
+++ public/Inside/buzz.php
@@ -231,7 +231,7 @@ if (isset($_POST['submit'])) {
     </script>
 </head>
 
-<body>
+<body class="page-body">
     <div class="page-container">
         <div class="sidebar-menu">
             <div class="sidebar-menu-inner" style="font-family: 'Montserrat', sans-serif">
