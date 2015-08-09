b008e6b111d36c14aa05365f42ad453f3817fb6c
root <root@ip-172-31-30-56.ap-southeast-1.compute.internal>
2015-08-09 12:39:22 +0000

    Removed login verification

--- public/Inside/buzz.php
+++ public/Inside/buzz.php
@@ -1,7 +1,6 @@
 <?php require_once("../../includes/session.php");?>
 <?php require_once("../../includes/db_connection.php");?>
 <?php require_once("../../includes/functions.php");?>
-<?php confirm_logged_in(); ?>
 <?php
 $event_set = find_all_events();
 ?>
