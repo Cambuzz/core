c884122d32a74cb47fad94f0bb70dfcc902df37b
Prastut <kr.prastut@gmail.com>
2015-08-24 21:58:34 +0530

    Fixing Track Teacher.php again

--- public/Inside/track_teacher.php
+++ public/Inside/track_teacher.php
@@ -376,6 +376,7 @@
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="assets/js/search/index.js"></script>
     <script src="assets/js/gsap/main-gsap.js"></script>
     <!-- Imported scripts on this page --> 
@@ -384,6 +385,11 @@
     <script src="assets/js/typeahead.js"></script>
     <script src="assets/js/modernizr.custom.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/classie/1.0.1/classie.min.js"></script>
+    <script src="assets/js/joinable.js"></script>
+    <script src="assets/js/resizeable.js"></script>
+    <script src="assets/js/uiMorphingButton_fixed.js"></script>
+    <script src="assets/js/style-api.js"></script>
+
 
     <script>
       var substringMatcher = function(strs) {
