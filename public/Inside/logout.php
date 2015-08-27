eb865878f141c14b5916988c8e0fbe770a42a96e
patelfenil <patelfenil1996@gmail.com>
2015-08-28 03:12:18 +0530

    working on count of live users

--- public/Inside/logout.php
+++ public/Inside/logout.php
@@ -2,13 +2,13 @@
 <?php require_once("../../includes/db_connection.php");?>
 <?php require_once("../../includes/functions.php");?>
 <?php
-session_start();
+//session_start();
 $_SESSION = array();
 if (isset($_COOKIE[session_name()])) {
 	setcookie(session_name(), '', time()-42000, '/');
 }
                 $username=$_SESSION["username"];
-                $query_delete="DELETE FROM live WHERE live_users='{$username}'";
+                $query_delete="DELETE FROM live WHERE live_users='{$username}' LIMIT 1";
                 $result_delete=mysqli_query($conn,$query_delete);
 session_destroy();
 redirect_to("../../index.php");
