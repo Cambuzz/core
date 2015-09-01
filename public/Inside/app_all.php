f7872d67af07647ebbf73c3d75974c2d4629a563
Prashant <pkpbhardwaj729@gmail.com>
2015-09-02 00:24:12 +0530

    app_all changed

--- public/Inside/app_all.php
+++ public/Inside/app_all.php
@@ -1,24 +1,9 @@
-<?php require_once("../../includes/session.php");?>
-<?php require_once("../../includes/db_connection.php");?>
-<?php require_once("../../includes/functions.php");?>
-
-<?php
-$event_set = find_all_events();
-?>
-<?php
-    $query = "SELECT * FROM notify ORDER BY id DESC";
-    $result = mysqli_query($conn, $query);
-    confirm_query($result);
-?>
-
-    <?php
-        while($notification = mysqli_fetch_assoc($result)) $output[]=$notification;
-        print(json_encode($output));
-    ?>                       
-
-<?php
-    
-    if (isset ($conn)){
-            mysqli_close($conn);
-    }
-?>
+<<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+ <b>Please Download the update.</b>
+</body>
+</html>
