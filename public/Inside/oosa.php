e90b266129b99c1e8e7522b4f12577b5c5dfb79d
Prashant <pkpbhardwaj729@gmail.com>
2015-09-02 19:57:31 +0530

    username changed

--- public/Inside/oosa.php
+++ public/Inside/oosa.php
@@ -20,7 +20,7 @@
     $picture_id = $current_user.$id_time;    
 ?>
 <?php
-if ($current_user=="12BEC1096") {
+if ($current_user=="cambuzz") {
     $view = " ";
 } else {
     $view = "style='display: none;'";
