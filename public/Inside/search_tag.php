1288723bf7e99480607933cbf328a371140d6010
patelfenil <patelfenil1996@gmail.com>
2015-09-03 03:07:16 +0530

    hashtags

--- public/Inside/search_tag.php
+++ public/Inside/search_tag.php
@@ -1,6 +1,6 @@
 <?php require_once("../../includes/session.php");?>
 <?php require_once("../../includes/db_connection.php");?>
-<?php require_once("../../includes/functions.php");?>);?>
+<?php require_once("../../includes/functions.php");?>
 
 
 <!DOCTYPE html>
