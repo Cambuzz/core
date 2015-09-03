80cc8fab79c19a8bafe965a9fc24ef44b7a9ea88
Prastut <kr.prastut@gmail.com>
2015-09-03 17:21:32 +0530

    Trying gif on OOSA

--- public/Inside/oosa.php
+++ public/Inside/oosa.php
@@ -530,6 +530,8 @@ confirm_query($result);
             case 'BMP':
             case 'PNG':
             case 'TIF':
+            case 'GIF'
+            case 'gif'
             
             break;
             default:
