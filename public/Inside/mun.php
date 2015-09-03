84d2b0501a25ba80d9071a9f17151c4971e54c67
Prashant <pkpbhardwaj729@gmail.com>
2015-09-04 00:20:39 +0530

    working on mun post

--- public/Inside/mun.php
+++ public/Inside/mun.php
@@ -37,7 +37,7 @@ if (($current_user=="12BEC1096")||($current_user=="cambuzz")||($current_user=="V
 <?php
 if (isset($_POST['submit'])) {
     if (isset($_POST['content'])) {
-        $content = $_POST['content'];
+        $content = mysqli_real_escape_string($conn,htmlspecialchars($_POST['content']));
     } else {
         $content = "";
     }
@@ -195,7 +195,7 @@ confirm_query($result);
                         </a>
                     </li>
                     <li>
-                            <a href="logout.php" class="visible-xs" id="phone-logout">
+                            <a href="logout.php" style="display: none;" id="phone-logout">
                                 <i class="entypo-logout"></i>
                                 <span class="title">Logout</span>
                             </a>
