ee2a83c0d0e46e96c513a01dc050d9ae792ccb55
patelfenil <patelfenil1996@gmail.com>
2015-09-03 22:31:24 +0530

    Adding superusers

--- public/Inside/arab_comments.php
+++ public/Inside/arab_comments.php
@@ -548,6 +548,7 @@ if ((isset($_POST['submit']))&&(isset($_POST['comment']))) {
                                                 <form method="post" action="arab_comments.php?id=<?php echo urlencode($_GET["id"]); ?>">
                                                     <textarea style="padding-right: 70px;" class="form-control autogrow" name="comment" value="" required placeholder="Write a comment..."></textarea>
                                                     <input type="submit" name="submit" class="btn btn-info" style="background-color: #333; color: white; margin-top: 2px;"> 
+                                                </form>
                                                 </div> </div>
                                             </li>
                                         </ul>
