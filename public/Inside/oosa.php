f3ca823035400de68a2423355eb7656d3d7435a8
Prastut <kr.prastut@gmail.com>
2015-09-05 06:25:07 +0530

    Trying Scaling Down Js

--- public/Inside/oosa.php
+++ public/Inside/oosa.php
@@ -301,19 +301,14 @@ confirm_query($result);
                                     <section class="profile-feed">
                                         <!-- profile post form -->
                                         <div <?php echo $view; ?> >
-                                        <form class="profile-post-form dropzone" id="dropzone_example" method="post" action="oosa.php" enctype="multipart/form-data">
+                                        <form class="profile-post-form" method="post" action="oosa.php" enctype="multipart/form-data">
                                             <textarea class="form-control autogrow" name="content" placeholder="UNOOSA POST"></textarea>
                                             <div class="form-options">
                                                 <div class="post-type">
-<!--                                                     <a href="#" class="tooltip-primary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Upload a Picture">
+                                                    <a href="#" class="tooltip-primary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Upload a Picture">
                                                     <input type="file" class="upload-picture"name="picture" accept=".jpeg, .jpg, .bmp, .png, .gif" id="picture"  style="display: none;">
                                                         <i class="entypo-camera" style="font-size: 20px;"></i>
                                                     </a>                                                    <a href="#" class="tooltip-primary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Attach a file">
- -->                                            
-                                                    <div class="fallback">
-                                                        <input name="file" type="file" name="picture" multiple />
-                                                    </div>    
-
                                                 </div>
                                                 <div class="post-submit">
                                                     <input type="submit" name="submit" value="Post" class="btn btn-success">
@@ -477,7 +472,6 @@ confirm_query($result);
             </footer>
         </div>
     </div>
-     <link rel="stylesheet" href="assets/js/dropzone/dropzone.css">
     <script src="assets/js/modernizr.custom.js"></script>
     <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -492,10 +486,6 @@ confirm_query($result);
     <script src="assets/js/resizeable.js"></script>
     <script src="assets/js/uiMorphingButton_fixed.js"></script>
     <script src="assets/js/style-api.js"></script>
-
-
-    <script src="assets/js/dropzone/dropzone.js"></script>
-
     <div class="modal" id="modal-1">
         <div class="modal-dialog">
             <div class="modal-content">
--- public/Inside/oosatest.php
+++ public/Inside/oosatest.php
@@ -306,10 +306,10 @@ confirm_query($result);
                                             <div class="form-options">
                                                 <div class="post-type">
                                                     <a href="#" class="tooltip-primary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Upload a Picture">
-                                                    <input type="file" class="upload-picture"name="picture" accept=".jpeg, .jpg, .bmp, .png, .gif" id="picture"  style="display: none;">
+                                                    <input type="file" class="upload-picture" name="picture" accept=".jpeg, .jpg, .bmp, .png, .gif" id="picture"  style="display: none;">
                                                         <i class="entypo-camera" style="font-size: 20px;"></i>
                                                     </a>               
-;                                                </div>
+                                                 </div>
                                                 <div class="post-submit">
                                                     <input type="submit" name="submit" value="Post" class="btn btn-success">
                                                 </div>
@@ -531,34 +531,116 @@ confirm_query($result);
             });
 
     </script>
+    <script>
+            var fileinput = document.getElementById('picture');
 
-    <script type="text/javascript">
-    var file = document.getElementById('picture');
+            var max_width = fileinput.getAttribute('data-maxwidth');
+            var max_height = fileinput.getAttribute('data-maxheight');
 
-    file.onchange = function(e){
-        var ext = this.value.match(/\.([^\.]+)$/)[1];
-        switch(ext)
-        {
-            case 'jpg':
-            case 'jpeg':
-            case 'bmp':
-            case 'png':
-            case 'tif':
-            case 'JPG':
-            case 'JPEG':
-            case 'BMP':
-            case 'PNG':
-            case 'TIF':
-            case 'GIF':
-            case 'gif':
-            
-            break;
-            default:
-            alert('File type not supported, please select an image file.');
-            this.value='';
-        }
-    };
+            var preview = document.getElementById('preview');
+
+            var form = document.getElementById('form');
+
+            function processfile(file) {
+              
+                if( !( /image/i ).test( file.type ) )
+                    {
+                        alert( "File "+ file.name +" is not an image." );
+                        return false;
+                    }
+
+                // read the files
+                var reader = new FileReader();
+                reader.readAsArrayBuffer(file);
+                
+                reader.onload = function (event) {
+                  // blob stuff
+                  var blob = new Blob([event.target.result]); // create blob...
+                  window.URL = window.URL || window.webkitURL;
+                  var blobURL = window.URL.createObjectURL(blob); // and get it's URL
+                  
+                  // helper Image object
+                  var image = new Image();
+                  image.src = blobURL;
+                  //preview.appendChild(image); // preview commented out, I am using the canvas instead
+                  image.onload = function() {
+                    // have to wait till it's loaded
+                    var resized = resizeMe(image); // send it to canvas
+                    var newinput = document.createElement("input");
+                    newinput.type = 'hidden';
+                    newinput.name = 'images[]';
+                    newinput.value = resized; // put result from canvas into new hidden input
+                    form.appendChild(newinput);
+                  }
+                };
+            }
+
+            function readfiles(files) {
+              
+                // remove the existing canvases and hidden inputs if user re-selects new pics
+                var existinginputs = document.getElementsByName('images[]');
+                var existingcanvases = document.getElementsByTagName('canvas');
+                while (existinginputs.length > 0) { // it's a live list so removing the first element each time
+                  // DOMNode.prototype.remove = function() {this.parentNode.removeChild(this);}
+                  form.removeChild(existinginputs[0]);
+                  preview.removeChild(existingcanvases[0]);
+                } 
+              
+                for (var i = 0; i < files.length; i++) {
+                  processfile(files[i]); // process each file at once
+                }
+                fileinput.value = ""; //remove the original files from fileinput
+                // TODO remove the previous hidden inputs if user selects other files
+            }
+
+            // this is where it starts. event triggered when user selects files
+            fileinput.onchange = function(){
+              if ( !( window.File && window.FileReader && window.FileList && window.Blob ) ) {
+                alert('The File APIs are not fully supported in this browser.');
+                return false;
+                }
+              readfiles(fileinput.files);
+            }
+
+            // === RESIZE ====
+
+            function resizeMe(img) {
+              
+              var canvas = document.createElement('canvas');
+
+              var width = img.width;
+              var height = img.height;
+
+              // calculate the width and height, constraining the proportions
+              if (width > height) {
+                if (width > max_width) {
+                  //height *= max_width / width;
+                  height = Math.round(height *= max_width / width);
+                  width = max_width;
+                }
+              } else {
+                if (height > max_height) {
+                  //width *= max_height / height;
+                  width = Math.round(width *= max_height / height);
+                  height = max_height;
+                }
+              }
+              
+              // resize the canvas and draw the image data into it
+              canvas.width = width;
+              canvas.height = height;
+              var ctx = canvas.getContext("2d");
+              ctx.drawImage(img, 0, 0, width, height);
+              
+              preview.appendChild(canvas); // do the actual resized preview
+              
+              return canvas.toDataURL("image/jpeg",0.7); // get the data from canvas as 70% JPG (can be also PNG, etc.)
+
+            }
+
+
     </script>
+
 </body>
 </html>
 <?php
