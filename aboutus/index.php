05af00aeab680e4e1e491bd596f14bfef24d636e
Prastut <kr.prastut@gmail.com>
2015-09-05 20:40:42 +0530

    Changed Prashant's About Us

--- aboutus/index.php
+++ aboutus/index.php
@@ -294,7 +294,7 @@
                                     <h1 style="color:white; padding-top: 20px;">Prashant Bhardwaj</h1>
                                     <div class="content" style="font-family: 'Montserrat'; color: white;">
                                         <p>Backend Developer</p>
-                                        <p>He is the one who the biggest chutiya. He loves Pragya Gupta i.e the Big Data. He is the biggest tharki of the team. 
+                                        <p>He is the one who made this team come around. A highly ambitious person, he makes sure that he sleeps the last and wakes up the first. You can find him either thinking of new ideas for starting up or singing along our team's anthem (*cringe*) And oh! Ask him to play Sardar Khan from the Wasseypur fame.
                                             <br><em>"Goongi ho kya"</em> xD
                                             <br>
                                             <br> P.s. He gets down in one shot. Period.
