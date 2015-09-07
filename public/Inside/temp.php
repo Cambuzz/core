d6df69a9a84269e893cd956c07f410358eefd635
patelfenil <patelfenil1996@gmail.com>
2015-09-08 02:12:59 +0530

    restoring mun

--- public/Inside/temp.php
+++ public/Inside/temp.php
@@ -2,19 +2,71 @@
 <?php require_once("../../includes/db_connection.php");?>
 <?php require_once("../../includes/functions.php");?>
 <?php
-$council="VITC INTRA MUN";
-$query="SELECT * FROM app WHERE council='{$council}' ORDER BY id ASC";
+
+$query="SELECT * FROM app ORDER BY id ASC";
 $result=mysqli_query($conn,$query);
 while($row=mysqli_fetch_assoc($result))
 {
-	$content=$row['content'];
-	$picset=$row['picset'];
-	$post_user=$row['post_user'];
-	$post_time=$row['post_time'];
-    $query1 = "INSERT INTO mun (content, picset, post_user, post_time) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}')";
-    $sql = mysqli_query($conn, $query1);
-    if($sql)
-    	echo "done";
+	if($row['council']=="VITC INTRA MUN")
+	{
+		$content=$row['content'];
+		$picset=$row['picset'];
+		$post_user=$row['post_user'];
+		$post_time=$row['post_time'];
+	    $query1 = "INSERT INTO mun (content, picset, post_user, post_time) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}')";
+	    $sql = mysqli_query($conn, $query1);
+	    if(!$sql)
+	    	echo "done";
+    }
+    else if($row['council']=="UNGA-ESS")
+	{
+		$content=$row['content'];
+		$picset=$row['picset'];
+		$post_user=$row['post_user'];
+		$post_time=$row['post_time'];
+	    $query1 = "INSERT INTO ess (content, picset, post_user, post_time) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}')";
+	    $sql = mysqli_query($conn, $query1);
+	    if(!$sql)
+	    	echo "done";
+    }
+    else if($row['council']=="UNOOSA")
+	{
+		$content=$row['content'];
+		$picset=$row['picset'];
+		$post_user=$row['post_user'];
+		$post_time=$row['post_time'];
+	    $query1 = "INSERT INTO oosa (content, picset, post_user, post_time) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}')";
+	    $sql = mysqli_query($conn, $query1);
+	    if(!$sql)
+	    	echo "done";
+    }
+    else if($row['council']=="UNHRC")
+	{
+		$content=$row['content'];
+		$picset=$row['picset'];
+		$post_user=$row['post_user'];
+		$post_time=$row['post_time'];
+	    $query1 = "INSERT INTO hrc (content, picset, post_user, post_time) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}')";
+	    $sql = mysqli_query($conn, $query1);
+	    if(!$sql)
+	    	echo "done";
+    }
+    else if($row['council']=="Arab League")
+	{
+		$content=$row['content'];
+		$picset=$row['picset'];
+		$post_user=$row['post_user'];
+		$post_time=$row['post_time'];
+	    $query1 = "INSERT INTO arab (content, picset, post_user, post_time) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}')";
+	    $sql = mysqli_query($conn, $query1);
+	    if(!$sql)
+	    	echo "done";
+    }
+    else
+    {
+    	echo "failed";
+    }
+
 	
 } 
 ?>
