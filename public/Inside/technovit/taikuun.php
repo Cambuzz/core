cc489b404d2bdf08c1ad1698664e97792bb91302
patelfenil <patelfenil1996@gmail.com>
2015-09-30 15:36:41 +0530

    trying to remove resubmission

--- public/Inside/technovit/taikuun.php
+++ public/Inside/technovit/taikuun.php
@@ -82,6 +82,7 @@ if (isset($_POST['submit'])) {
     $main_query = "INSERT INTO app_technovit (content, picset, post_user, post_time, school, po_url, dp_url) VALUES ('{$content}', {$picset}, '{$post_user}', '{$post_time}', '{$school}', '{$po_url}', '{$dp_url}')";      
     $main_sql = mysqli_query($conn, $main_query);
     unset($_POST);
+     redirect_to("taikuun.php");
 }
 
 if((isset($_POST['searchsubmit']))&&(isset($_POST['search'])))
