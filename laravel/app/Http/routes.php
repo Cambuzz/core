52e1990d25bc897b8876d3c5822e520e95da2347
patelfenil <patelfenil1996@gmail.com>
2015-10-24 21:55:24 +0530

    carbon int

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -128,7 +128,7 @@ Route::post('confirmation', function()
     $ectstamp=$user->ectstamp;
     $confirmed=$user->confirmed;
     $verifytime = Carbon\Carbon::now();
-    if((int)$verifytime<=(((int)$ectstamp)+1800))
+    if(($verifytime-$ectstamp)<=(1800)
     {
 	    if($confirmed==0)
 	    {
