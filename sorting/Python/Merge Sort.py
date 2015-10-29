b9ebdde02fc4c71667878b0a4c75909af26c1792
Prastut <kr.prastut@gmail.com>
2015-10-30 00:09:55 +0530

    Python code uploaded

---
+++ sorting/Python/Merge Sort.py
@@ -0,0 +1,43 @@
+import time
+import random
+def mergeSort(alist):
+    #print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+   # print("Merging ",alist)
+for i in range (0,10):
+    alist = []
+    for j in range(0,50000):
+        alist.append(int(random.random()*1000000))
+    t1=time.time()
+    mergeSort(alist)
+    t2=time.time()
+
+    print(t2-t1)
