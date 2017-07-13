c08b0a63ed9c5d08e82f785da1c2bbf9b9f31df4
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-07-13 17:37:54 +0530

    seq change

--- learnpro/login.php
+++ learnpro/login.php
@@ -9,7 +9,7 @@
     mysqli_stmt_execute($statement);
     
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $name, $age, $username, $password);
+    mysqli_stmt_bind_result($statement, $userID, $name, $username, $age,  $password);
     
     $response = array();
     $response["success"] = false;  
